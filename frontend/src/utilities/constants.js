57bad92912710b5a0d5cbeb21dc49c8c44a89d3b
Suvrat <suvratmittal007@gmail.com>
2025-10-12 21:18:20 +0530

    testing

--- frontend/src/utilities/constants.js
+++ frontend/src/utilities/constants.js
@@ -1,2 +1,3 @@
 // export const BASE_URL = 'http://localhost:8000/';
-export const BASE_URL = 'https://tutora-1.onrender.com';
+export const BASE_URL = '';
+// export const BASE_URL = 'https://tutora-backend.vercel.app/';
